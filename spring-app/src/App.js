d9ba089957b5840efd9734cca95cc0409118a5df
Raghavendra <raghavendrardeshabhandari@gmail.com>
2023-09-22 13:55:27 +0530

    BottomSheet component added

--- spring-app/src/App.js
+++ spring-app/src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import { BottomSheet } from './components/BottomSheet/BottomSheet';
-import { Hello } from './components/Hello';
 
 function App() {
   return (
     <div className="App">
-      <BottomSheet >
-        <Hello />
+      <BottomSheet>
       </BottomSheet>
     </div>
   );
